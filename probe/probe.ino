75e1c8b85416eb84e97559949cfbaa4189649738
Sergi Mus <sergi.mus@gmail.com>
2022-07-08 14:04:51 +0200

    Modified probe to accept LCD

--- probe/probe.ino
+++ probe/probe.ino
@@ -1,11 +1,21 @@
 #include <SPI.h>
 #include <SD.h>
+#include <LiquidCrystal.h>
+
+#define LCD_RS 10
+#define LCD_EN 9
+#define LCD_D4 8
+#define LCD_D5 7
+#define LCD_D6 6
+#define LCD_D7 5
 
 #define HUMIDITY_SENSOR_ID 1
 #define HUMIDITY_SAMPLING_INTERVAL 3000
 #define HUMIDITY_SENSOR_PIN A0
 
 #define SD_CHIP_SELECT 4
+
+#define BUFFER_CAPACITY 75
 
 template<typename T, int N>
 class Queue
@@ -92,22 +102,32 @@ class SensorData
     }
 };
 
-
-Queue<SensorData, 50> dataBuffer;
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+Queue<SensorData, BUFFER_CAPACITY> dataBuffer;
 
 void readSensor()
 {
   if (!dataBuffer.full())
   {
-    SensorData data(analogRead(HUMIDITY_SENSOR_PIN), HUMIDITY_SENSOR_ID);
+    int sensorValue = analogRead(HUMIDITY_SENSOR_PIN);
+    SensorData data(sensorValue, HUMIDITY_SENSOR_ID);
     dataBuffer.push(data);
+    int humidity = map(sensorValue, 0, 1024, 100, 0);
+    lcd.clear();
+    lcd.print("H:");
+    lcd.print(humidity);
+    lcd.print("%");
+    lcd.setCursor(7, 0);
+    lcd.print("SV:");
+    lcd.print(sensorValue);
+    lcd.setCursor(0, 1);
+    lcd.print("B:");
+    lcd.print(dataBuffer.count());
+    lcd.setCursor(7, 1);
+    lcd.print("FT:");
+    lcd.print((BUFFER_CAPACITY - dataBuffer.count() - 1) * HUMIDITY_SAMPLING_INTERVAL / 1000);
+    lcd.print("s");
   }
-}
-
-void logBuffer()
-{
-  Serial.print("Buffer occupation: ");
-  Serial.println(dataBuffer.count());
 }
 
 void writeData()
@@ -136,14 +156,13 @@ void writeData()
 }
 
 RepeatingTask tasks[] = {
-  RepeatingTask(3000, readSensor),
-  RepeatingTask(6000, logBuffer),
-  RepeatingTask(24000, writeData)
+  RepeatingTask(HUMIDITY_SAMPLING_INTERVAL, readSensor),
+  RepeatingTask(30000, writeData)
 };
 
 void setup()
 {
-  Serial.begin(9600);
+  lcd.begin(16, 2);
 }
 
 void loop()
